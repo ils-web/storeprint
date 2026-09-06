42a5ba3da310bcfd98281379df1d62b199ec64ee
ils-web <home156p@gmail.com>
2026-09-06 08:44:35 +0300

    Connect real-time Cloud Firestore database storeprint-8fab1

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
 const FIREBASE_CONFIG = {
-  projectId: "zeta-sanctum-0j4jh",
-  appId: "1:1080969786292:web:f351e432beb1eceef278e1",
-  apiKey: "AIzaSyCYBC6H-afVLv1ILxqSmEXXXLeFHHu4W30",
-  authDomain: "zeta-sanctum-0j4jh.firebaseapp.com",
-  storageBucket: "zeta-sanctum-0j4jh.firebasestorage.app",
-  messagingSenderId: "1080969786292",
+  projectId: "storeprint-8fab1",
+  appId: "1:796973894702:web:e20b822681117494f423bf",
+  apiKey: "AIzaSyAvk9urcRChSpRge0az1Wx59EfzpKR6A3Q",
+  authDomain: "storeprint-8fab1.firebaseapp.com",
+  storageBucket: "storeprint-8fab1.firebasestorage.app",
+  messagingSenderId: "796973894702",
+  measurementId: "G-VQHD2GVQKR",
 };
 
 export const app = getApps().length === 0 
